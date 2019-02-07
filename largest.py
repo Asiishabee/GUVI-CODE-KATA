3dc0c0c6e789dee9997ba50e8db7df27591849af
Asiishabee <42670433+Asiishabee@users.noreply.github.com>
2019-02-07 13:38:46 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,23 @@
+while True:
+	try:
+		a,b,c=input("").split()
+		a=int(a)
+		b=int(b)
+		c=int(c)
+		break
+	except:
+		print("invalid")
+		break
+if(a>b):
+		if(a>c):
+			print(a)
+	
+		elif c>a:
+			print(c)
+elif(b>a):
+		if(b>c):
+			print(b)
+		elif c>b:
+			print(c)
+			
+		
